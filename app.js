a3cdf0bb107e0420adba7c632bb12c6333bc23c7
Leon Norris <lnorris76@gmail.com>
2017-12-19 05:04:46 -0800

    Delete logic added

--- app.js
+++ app.js
@@ -31,6 +31,19 @@ function onReady() {
     newToDoText.value = '';
 
     //add delete button to delete To Dos not needed
+    function deleteRow(tableID) {
+			try {
+			var table = document.getElementById(tableID);
+			var rowCount = table.rows.length;
+
+			for(var i=0; i<rowCount; i++) {
+				var row = table.rows[i];
+				var chkbox = row.cells[0].childNodes[0];
+				if(null != chkbox && true == chkbox.checked) {
+					table.deleteRow(i);
+					rowCount--;
+					i--;
+				}
 
   });
 }
